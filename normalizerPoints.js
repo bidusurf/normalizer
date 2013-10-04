af212656eeaabfb2dd9baee5d6101ac182b7b0b5
bidusurf <bidusurf@gmail.com>
2013-10-04 01:31:03 -0300

    New approach with bind

--- normalizerPoints.js
+++ normalizerPoints.js
@@ -5,20 +5,9 @@ var mysql = require('mysql');
 var readline = require('readline');
 var async = require('async');
 
-var openStatements = 0;
-
-var decreaseStatementsAndCloseIfRequired = function() {
-	openStatements--;
-	console.log(openStatements);
-	if (openStatements === 0) {
-		connection.end();
-	}
-};
-
-var increaseStatements = function() {
-	openStatements++;
-	console.log(openStatements);
-};
+var conns = 0;
+var points = 0;
+var processingObject = false;
 
 var parseTimestamp = function(fromFile) {
 	var splited = fromFile.split(' ');
@@ -32,70 +21,92 @@ var parseTimestamp = function(fromFile) {
 					timeSplited[2]);
 };
 
+var instPointFunction = function(trajectoryId, baseDir, trajectoryFiles, idx, err, insertPointConn) {
+		  			if (err) throw err;
+					var rd = readline.createInterface({
+			    		input: fs.createReadStream(baseDir + trajectoryFiles[idx]),
+			    		output: process.stdout,
+			    		terminal: false
+					});
+
+					rd.on('close', function(){
+						if (idx == trajectoryFiles.length - 1) {
+					  		insertPointConn.destroy();
+						}
+					});
+
+					var lines = 0;
+					rd.on('line', function(line) {
+						if (lines < 6) {
+							lines++;
+							return;
+						}
+
+						var lineValues = line.split(',');
+						var latitude = lineValues[0];
+						var longitude = lineValues[1];
+						var altitude = lineValues[2];
+						var timestamp = lineValues[5] + ' ' + lineValues[6];
+
+						console.log(++points);
+						var sql = 'INSERT INTO RawPoint SET idRawTrajectory = ' + trajectoryId + 
+							', the_geom = GeomFromText("POINT('+ latitude + ' ' + longitude + ')")' +
+							', timestamp = "' + timestamp + '"';
+						insertPointConn.query(sql, function(err, result) {
+				  			if (err) throw err;
+				  		});
+					});
+				};
+
 var parseTrajectories = function(objectId, baseDir, dataDir) {
-	increaseStatements();
-	var rawTrajectoryValues = {idObject: objectId};
-	connection.query('INSERT INTO RawTrajectory SET ?', rawTrajectoryValues, function(err, result) {
+	console.log(++conns);
+	pool.getConnection(function(err, insertTrajectoryConn) {
   		if (err) throw err;
 
-		var trajectoryFiles = fs.readdirSync(baseDir + '/' + dataDir + '/Trajectory');
-		for (var i = trajectoryFiles.length - 1; i >= 0; i--) {
+		var rawTrajectoryValues = {idObject: objectId};
+		insertTrajectoryConn.query('INSERT INTO RawTrajectory SET ?', rawTrajectoryValues, function(err, result) {
+	  		if (err) throw err;
 
-			var rd = readline.createInterface({
-	    		input: fs.createReadStream(baseDir + '/' + dataDir +'/Trajectory/' + trajectoryFiles[i]),
-	    		output: process.stdout,
-	    		terminal: false
-			});
+	  		insertTrajectoryConn.destroy();
+			var trajectoryFiles = fs.readdirSync(baseDir + '/' + dataDir + '/Trajectory');
+			for (var i = 0; i < trajectoryFiles.length; i++) {
 
-			var lines = 0;
-			rd.on('line', function(line) {
-				if (lines < 6) {
-					lines++;
-					return;
-				}
-
-				var lineValues = line.split(',');
-				var latitude = lineValues[0];
-				var longitude = lineValues[1];
-				var altitude = lineValues[2];
-				var timestamp = new Date(lineValues[5] + ' ' + lineValues[6]);
-				increaseStatements();
-				var sql = 'INSERT INTO RawPoint SET idRawTrajectory = ' + result.insertId + 
-					' the_geom = GeomFromText("POINT('+ latitude + ' ' + longitude + ')")' +
-					' timestamp = ' + timestamp;
-				connection.query(sql, function(err, result) {
-		  			if (err) throw err;
-			  		decreaseStatementsAndCloseIfRequired();
-		  		});
-			});
-
-	  	}
-		decreaseStatementsAndCloseIfRequired();
+				console.log(++conns);
+				var fileName = trajectoryFiles[i];
+				var idx = i;
+				pool.getConnection(instPointFunction.bind(undefined, result.insertId, baseDir + '/' + dataDir +'/Trajectory/', trajectoryFiles, idx));
+		  	}
+		});
 	});
 };
 
 var parseObjects = function (baseDir, dataDir) {
-	increaseStatements();
-	connection.query('SELECT * FROM Object WHERE name = ?', [dataDir], function(err, rows, fields) {
+	console.log(++conns);
+	pool.getConnection(function(err, selectObjectConn) {
   		if (err) throw err;
 
-  		if (rows.length > 0) {
-	  		parseTrajectories(rows[0].idObject, baseDir, dataDir);
+		selectObjectConn.query('SELECT * FROM Object WHERE name = ?', [dataDir], function(err, rows, fields) {
+	  		if (err) throw err;
 
-  			decreaseStatementsAndCloseIfRequired();
-  		}
+	  		selectObjectConn.destroy();
+	  		if (rows.length > 0) {
+		  		parseTrajectories(rows[0].idObject, baseDir, dataDir);
+	  		}
+		});
 	});
 };
 
 var readAsDir = function (dirName) {
 	var dataDir = fs.readdirSync(dirName);
 	for (var i = dataDir.length - 1; i >= 0; i--) {
+		while (processingObject);
+		processingObject = true;
 		parseObjects(dirName, dataDir[i]);
 	};
 	
 };
 
-var connection = mysql.createConnection({
+var pool = mysql.createPool({
   host : 'localhost',
   database: 'tcc',
   user : 'pedro',
@@ -103,10 +114,5 @@ var connection = mysql.createConnection({
   port: 3306
 });
 
-connection.connect(function (err) {
-	if (err) {
-		console.log(err);
-	}
-});
-
-readAsDir('/home/pedro/desenvolvimento/Geolife Trajectories 1.2/Data');
+parseObjects('/home/pedro/desenvolvimento/Geolife Trajectories 1.2/Data', process.argv[2]);
+// readAsDir('/home/pedro/desenvolvimento/Geolife Trajectories 1.2/Data');
